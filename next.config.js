0c89abaa112d8ab658542c52391f64f91eb4e691
manojsagrowinfotech <manojsagrowinfotech@gmail.com>
2026-01-13 01:49:53 +0530

    remove production-ready CSP headers

--- next.config.js
+++ next.config.js
@@ -1,56 +1,11 @@
-/** @type {import('next').NextConfig} */
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'wasm-unsafe-eval' https://www.sagrowinfotech.com;
-  script-src-elem 'self' 'wasm-unsafe-eval' https://www.sagrowinfotech.com;
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' data: blob:;
-  font-src 'self';
-  connect-src 'self' https://sagrowinfotech-backend.onrender.com;
-  worker-src 'self' blob:;
-  frame-src 'self';
-`;
 
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
 
   async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: "https://sagrowinfotech-backend.onrender.com/api/:path*",
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\n/g, ""),
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
       },
     ];
   },
